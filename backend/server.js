e409724547a5db4bab9dc83e0e537f903d4f2ab4
kandula sravya <sravyakandula194@gmail.com>
2026-05-20 12:06:56 +0530

    Configure CORS for specific origin and methods

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://week8-project-xi.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 /* CONNECT ROUTES */
